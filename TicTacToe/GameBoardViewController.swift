812758c0ea383494de078460c095582b57a504be
Manas <manas@zopsmart.com>
2020-01-09 11:59:26 +0530

    Initial commit

---
+++ TicTacToe/GameBoardViewController.swift
@@ -0,0 +1,36 @@
+//
+//  GameBoardViewController.swift
+//  TicTacToe
+//
+//  Created by Manas Aggarwal on 08/01/20.
+//  Copyright © 2020 Manas Aggarwal. All rights reserved.
+//
+
+import UIKit
+
+class GameBoardViewController: UIViewController {
+
+    
+    @IBOutlet weak var btn1: UIButton!
+    @IBOutlet weak var btn2: UIButton!
+    @IBOutlet weak var btn3: UIButton!
+    @IBOutlet weak var btn4: UIButton!
+    @IBOutlet weak var btn5: UIButton!
+    @IBOutlet weak var btn6: UIButton!
+    @IBOutlet weak var btn7: UIButton!
+    @IBOutlet weak var btn8: UIButton!
+    @IBOutlet weak var btn9: UIButton!
+    @IBOutlet weak var startBtn: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    
+    @IBAction func startBtnTapped(_ sender: UIButton) {
+    }
+    
+    
+}
